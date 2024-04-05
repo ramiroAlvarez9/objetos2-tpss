de6dd291eea44dcc29cf0d07991f6acf7ff7a308
ramiroAlvarez9 <ramiro.al.alvarez@gmail.com>
2024-04-05 16:17:26 -0300

    commit para push

--- TP4/src/supermercado/ProductoPrimeraNecesidad.java
+++ TP4/src/supermercado/ProductoPrimeraNecesidad.java
@@ -1,13 +1,27 @@
 package supermercado;
 
-public class ProductoPrimeraNecesidad extends Producto{
+/*
+ * 
+Suponga ahora que el descuento a aplicar en cada producto de primera necesidad puede ser distinto y se debe poder definir al momento de
+crear el mismo. Por ejemplo, el arroz puede ser un producto de primera necesidad con un descuento del 8%, mientras que leche podría ser otro
+producto de primera necesidad con un decuento del 11%. Esto es sólo un ejemplo. El descuento a aplicar en cada producto de primera
+necesidad debe ser configurable al momento de crearlo.
 
-	public ProductoPrimeraNecesidad(String unNombre, double precio, boolean esDePreciosCuidados) {
-		super(unNombre,  precio, esDePreciosCuidados);
+
+1. Modifique los tests para contemplar esta variación.
+2. Implemente la solución en Java.
+
+*/
+public class ProductoPrimeraNecesidad extends Producto {
+
+	public ProductoPrimeraNecesidad(String unNombre, double precio, boolean esDePreciosCuidados, double descuentoParaAplicar) {
+		
+		super(unNombre, precio, esDePreciosCuidados);
+		this.precio = this.precio * descuentoParaAplicar;
 	
-		precio = precio * 0.9;
 	}
 	
 	
+
 
 }
--- TP4/test/supermercado/ProductoPrimeraNecesidadTest.java
+++ TP4/test/supermercado/ProductoPrimeraNecesidadTest.java
@@ -12,7 +12,7 @@ public class ProductoPrimeraNecesidadTest {
 	
 	@BeforeEach
 	public void setUp() {
-		leche = new ProductoPrimeraNecesidad("Leche", 8d, false);
+		leche = new ProductoPrimeraNecesidad("Leche", 8d, false, 0.9);
 	}
 	
 	@Test
